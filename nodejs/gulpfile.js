11697168cbab816512b94673030ac22ca05183d5
soujiro27 <jc.raus27@gmail.com>
2016-11-12 11:34:48 -0600

    usandopage

--- nodejs/gulpfile.js
+++ nodejs/gulpfile.js
@@ -1,5 +1,20 @@
 var gulp=require('gulp');
 var stylus=require('gulp-stylus');
+var rename=require('gulp-rename');
+var browserify=require('browserify');
+ var vinyl=require('vinyl-source-stream');
+var babel =require('babelify');
+
+
+
+
+gulp.task('assets',function(){
+	gulp
+		.src('assets/*')
+		.pipe(gulp.dest('public'));
+	});
+
+
 
 gulp.task('css',function(){
 
@@ -7,5 +22,23 @@ gulp.task('css',function(){
 	.pipe(stylus({
 		'include css':true
 	}))
+	.pipe(rename('app.css'))
 	.pipe(gulp.dest('./public/'))
 });
+
+gulp.task('js',function(){
+	browserify('./sources/index.js')
+	.transform(babel)
+	.bundle()
+	.pipe(vinyl('index.js'))
+	.pipe(rename('app.js'))
+	.pipe(gulp.dest('public'))
+	});
+
+
+
+
+gulp.task('default',['css','assets','js']);
+
+
+
--- nodejs/server.js
+++ nodejs/server.js
@@ -2,6 +2,8 @@ var express=require('express');
 var app=express();
 app.set('view engine','pug');
 
+app.use(express.static('public'));
+
 app.listen(3000,function(){
 	console.log("servidor corriendo");
 });
@@ -9,6 +11,11 @@ app.listen(3000,function(){
 app.get('/',function(req, res){
 	res.render('index');
 });
+
+app.get('/login',function(req, res){
+	res.render('index');
+});
+
 
 
 
